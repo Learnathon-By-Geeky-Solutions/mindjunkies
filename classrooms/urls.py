9dcbbff9b7ec36d236578e11dd08c48054b38ad5
SaimaLearnathon <saimafarhanaislam@gmail.com>
2025-02-04 19:15:05 +0600

    add classroom created

--- classrooms/urls.py
+++ classrooms/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('classroom_details', views.classroom_details, name='classroom_details'),
+    path('create_classroom',views.createClassroom,name='create_classroom')
 ]
