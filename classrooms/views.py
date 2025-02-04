e30c95b67709b7ea401a57469ce2c53a62fbc1c1
SaimaLearnathon <saimafarhanaislam@gmail.com>
2025-02-04 19:15:05 +0600

    add classroom created

--- classrooms/views.py
+++ classrooms/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+
+from .forms import ClassroomForm
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def createClassroom(request):
+    if request.method=='POST':
+        forms=ClassroomForm(request.POST or None)
+        if forms.is_valid():
+            forms.save()
+            return redirect('classroom_details')
+    else:
+        forms=ClassroomForm()
+    return render(request,'classrooms/create_classroom.html',{'forms':forms})        
+
+
 def classroom_details(request):
     return render(request, 'classrooms/classroom.html')
+
