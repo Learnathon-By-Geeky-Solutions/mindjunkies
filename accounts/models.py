e4c3f73f07c0ea6e30da3e7c79b0725c7e08d468
Shafayet Sadi <shafayet.sadi@gmail.com>
2025-02-05 19:57:50 +0600

    added admin option to user

--- accounts/models.py
+++ accounts/models.py
@@ -1,14 +1,33 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 import uuid
 
 from core.models import BaseModel
 
 
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('role', 'admin')
+
+        return self.create_user(username, email, password, **extra_fields)
+
+
 class User(AbstractUser):
     ROLE_CHOICES = [
         ('student', 'Student'),
         ('teacher', 'Teacher'),
+        ('admin', 'Admin'),
     ]
 
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -16,12 +35,14 @@ class User(AbstractUser):
 
     REQUIRED_FIELDS = ["email", "first_name", "last_name"]
 
+    objects = UserManager()
+
     def __str__(self):
         return f"{self.username} - {self.email}"
 
 
 class Profile(BaseModel):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    user = models.OneToOneField('accounts.User', on_delete=models.CASCADE, related_name='profile')
     birthday = models.DateField(null=True, blank=True)
     bio = models.TextField()
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
