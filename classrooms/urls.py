cb58031215b6ab208e7b7dc81bf30e0c3d0ff490
SaimaLearnathon <saimafarhanaislam@gmail.com>
2025-02-06 14:56:56 +0600

    edit classroom created

--- classrooms/urls.py
+++ classrooms/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('classroom_details', views.classroom_details, name='classroom_details'),
-    path('create_classroom',views.createClassroom,name='create_classroom')
+    path('classroom_details/<str:slug>', views.classroom_details, name='classroom_details'),
+    path('create_classroom',views.createClassroom,name='create_classroom'),
+    path('edit_classroom', views.edit_classroom, name='edit_classroom')
 ]
