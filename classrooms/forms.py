1b3af0dce07cbc5d7764be07640d954ac0efc689
SaimaLearnathon <saimafarhanaislam@gmail.com>
2025-02-06 14:56:56 +0600

    edit classroom created

--- classrooms/forms.py
+++ classrooms/forms.py
@@ -8,3 +8,12 @@ class ClassroomForm(forms.ModelForm):
                   'course_image', 'preview_video_link', 'published', 
                   'upcoming', 'published_on', 'paid_course', 
                   'course_price', 'slug', 'total_rating', 'number_of_ratings']
+    def save(self, commit=True):
+        instance = super(ClassroomForm, self).save(commit=False)
+        instance.slug = instance.title.lower().replace(' ', '-')
+        if commit:
+            instance.save()
+            return instance
+        
+class RatingForm(forms.Form):
+    rating = forms.IntegerField(min_value=1, max_value=5)                
--- classrooms/views.py
+++ classrooms/views.py
@@ -1,13 +1,15 @@
 
 from .forms import ClassroomForm
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
+from .models import Classroom
 
 # Create your views here.
 @login_required
 def createClassroom(request):
     if request.method=='POST':
-        forms=ClassroomForm(request.POST or None)
+        forms=ClassroomForm(request.POST, request.FILES)
+        
         if forms.is_valid():
             forms.save()
             return redirect('home')
@@ -16,6 +18,27 @@ def createClassroom(request):
     return render(request,'classrooms/create_classroom.html',{'forms':forms})        
 
 
-def classroom_details(request):
-    return render(request, 'classrooms/classroom.html')
+def classroom_details(request,slug):
+    classroom_detail=get_object_or_404(Classroom,slug=slug)
+    return render(request, 'classrooms/classroom.html',{'classroom_detail':classroom_detail})
+
+
+
+
+def edit_classroom(request):
+    slug = request.GET.get('slug')
+    classroom_detail = get_object_or_404(Classroom, slug=slug)
+    
+    if request.method == 'POST':
+        forms = ClassroomForm(request.POST, request.FILES, instance=classroom_detail)
+        if forms.is_valid():
+            forms.save()
+            # After saving, redirect to a success page or classroom details page
+            return redirect('classroom_details', slug=classroom_detail.slug)
+    else:
+        forms = ClassroomForm(instance=classroom_detail)
+    
+    return render(request, 'classrooms/edit_classroom.html', {'forms': forms, 'classroom_detail': classroom_detail})
+
+
 
