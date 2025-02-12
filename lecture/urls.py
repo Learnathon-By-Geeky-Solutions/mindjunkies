f155dbb8cc65d118347e07323d2ab70297c5f433
SaimaLearnathon <saimafarhanaislam@gmail.com>
2025-02-12 16:20:38 +0600

    lecture create page created

--- lecture/urls.py
+++ lecture/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views 
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns=[
     path('home',views.lecture_home ,name="lecture_home"),
     path('create_lecture',views.create_lecture,name="create_lecture")
