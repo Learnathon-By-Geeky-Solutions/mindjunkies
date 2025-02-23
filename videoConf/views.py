2f94794b4697c4ef1685f36c560619b1db316341
md-tonmoy007 <tonmoyhossainjifat313@gmail.com>
2025-02-23 15:47:04 +0600

    fixed merge error in vdoConf branch

--- videoConf/views.py
+++ videoConf/views.py
@@ -10,15 +10,16 @@ import time
 @require_http_methods(["GET"])
 @login_required
 def meeting(request: HttpRequest) -> HttpResponse:
-
-    
-
     context = { 
-        'room_name': 'room is awesome',
+        'room_name': f"{settings.JITSI_ROOM_PREFIX}{request.user.name}_{int(time.time())}",
+        'user_name': request.user.username,
         'JITSI_MAGIC_COOKIE': settings.JITSI_MAGIC_COOKIE,
         'SRI_HASH': settings.SRI_HASH,
-        'display_name': 'anonymous',
-        'jwt': settings.JWT,
         }
 
-    return render(request, "meet/index.html", context)
+    response = render(request, 'meet/index.html', context)
+    response['Content-Security-Policy'] = "frame-ancestors 'self' 8x8.vc"
+    response['X-Frame-Options'] = 'SAMEORIGIN'
+    
+    return response
+
