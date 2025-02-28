ae77c40db1d1381f9680002ad28f972a4a2e0fd5
Shafayet Sadi <shafayet.sadi@gmail.com>
2025-02-28 17:12:15 +0600

    for deployment: added theme/static

--- project/settings.py
+++ project/settings.py
@@ -151,7 +151,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]  # Ensure this exists
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "theme/static"),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
