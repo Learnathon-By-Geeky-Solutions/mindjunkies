e2a5941d36bfe6a9a768250f7aad10912f71cb90
SaimaLearnathon <saimafarhanaislam@gmail.com>
2025-03-02 21:04:00 +0600

    content added

--- lecture/urls.py
+++ lecture/urls.py
@@ -6,7 +6,9 @@ from django.conf import settings
 urlpatterns = [
     path('home/<str:slug>', views.lecture_home, name="lecture_home"),
     path('video/<str:module_id>/<str:video_id>',views.lecture_video,name="lecture_video_content"),
-    path('pdf/<str:slug>/<str:pdf_id>',views.lecture_pdf,name="lecture_pdf_content"),
-    path('create',views.create_lecture,name="create_lecture")
+    path('create/<str:slug>',views.create_lecture,name="create_lecture"),
+    path('create/content/<str:course_slug>/<str:lecture_slug>',views.create_content,name="create_content")
+
+   
   
 ]
