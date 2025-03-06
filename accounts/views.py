a41187f3e66e8deae43849d5d74eb5f34e2860c8
Shafayet Sadi <shafayet.sadi@gmail.com>
2025-03-06 19:38:04 +0600

    sonarqube: accounts/views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,27 +1,31 @@
 from django.contrib import messages
-from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import DetailView, UpdateView, TemplateView
 from django.shortcuts import render, get_object_or_404, redirect
+from django.utils.decorators import method_decorator
+from django.views.decorators.http import require_GET
+from django.urls import reverse_lazy
 
 from .forms import ProfileUpdateForm
 from .models import Profile
 
 
-def profile(request: HttpRequest) -> HttpResponse:
-    return render(request, 'profile.html')
 
 
-def edit_profile(request: HttpRequest) -> HttpResponse:
-    user_profile = get_object_or_404(Profile, user=request.user)
-    if request.method == "POST":
-        form = ProfileUpdateForm(request.POST, instance=user_profile)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Profile updated successfully!")
-            return redirect('profile')
-    else:
-        form = ProfileUpdateForm(instance=user_profile)
+@method_decorator(require_GET, name='dispatch')
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'profile.html'
 
-    context = {
-        "form": form
-    }
-    return render(request, 'edit_profile.html', context)
+
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+    model = Profile
+    template_name = 'edit_profile.html'
+    form_class = ProfileUpdateForm
+    success_url = reverse_lazy('profile')
+
+    def form_valid(self, form):
+        messages.success(self.request, "Profile updated successfully!")
+        return super().form_valid(form)
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(Profile, user=self.request.user)
--- project/settings.py
+++ project/settings.py
@@ -147,7 +147,6 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "theme" / "static",  # Correct path to the static directory inside the theme app
-    BASE_DIR / "static",
 ]
 STATIC_ROOT = BASE_DIR / "static"
 
