eead79b566c1199ead087c06bc8d4867e2895c20
Shafayet Sadi <shafayet.sadi@gmail.com>
2025-03-06 22:36:29 +0600

    sonarqube: liveclasses

--- liveclasses/views.py
+++ liveclasses/views.py
@@ -4,62 +4,50 @@ from django.contrib import messages
 from django.utils.timezone import now
 from .models import LiveClass
 from courses.models import Course
-
 import uuid
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
+from django.views.generic import ListView, CreateView, View
 
 
-@login_required
-def list_live_classes(request, slug):
-    """List all upcoming and ongoing live classes for a course."""
+class LiveClassListView(LoginRequiredMixin, ListView):
+    model = LiveClass
+    template_name = "liveclasses/list_live_classes.html"
+    context_object_name = "live_classes"
 
-    course = get_object_or_404(Course, slug=slug)
-    live_classes = LiveClass.objects.filter(course=course)
+    def get_queryset(self):
+        course = get_object_or_404(Course, slug=self.kwargs['slug'])
+        return LiveClass.objects.filter(course=course)
 
-    teacher = course.teachers.filter(teacher=request.user).exists()
-
-    return render(
-        request,
-        "liveclasses/list_live_classes.html",
-        {
-            "course": course,
-            "live_classes": live_classes,
-            "teacher": teacher,
-        }
-    )
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        course = get_object_or_404(Course, slug=self.kwargs['slug'])
+        context['course'] = course
+        context['teacher'] = course.teachers.filter(teacher=self.request.user).exists()
+        return context
 
 
-@login_required
-def create_live_class(request, slug):
-    """Allows a teacher to create a live class."""
-    course = get_object_or_404(Course, slug=slug)
+class CreateLiveClassView(LoginRequiredMixin, CreateView):
+    model = LiveClass
+    template_name = "liveclasses/create_live_class.html"
+    fields = ['topic', 'scheduled_at', 'duration']
 
-    if request.method == "POST":
-        topic = request.POST.get("topic")
-        scheduled_at = request.POST.get("scheduled_at")
-        duration = request.POST.get("duration")
+    def form_valid(self, form):
+        form.instance.teacher = self.request.user
+        form.instance.course = get_object_or_404(Course, slug=self.kwargs['slug'])
+        if LiveClass.objects.filter(teacher=self.request.user, scheduled_at=form.instance.scheduled_at).exists():
+            messages.error(self.request, "You already have a class scheduled at this time!")
+            return redirect("create_live_class", slug=self.kwargs['slug'])
+        messages.success(self.request, "Live class created successfully!")
+        return super().form_valid(form)
 
-        if LiveClass.objects.filter(teacher=request.user, scheduled_at=scheduled_at).exists():
-            messages.error(request, "You already have a class scheduled at this time!")
-            return redirect("create_live_class", slug=course.slug)
-
-        LiveClass.objects.create(
-            course=course,
-            teacher=request.user,
-            topic=topic,
-            scheduled_at=scheduled_at,
-            duration=duration
-        )
-
-        messages.success(request, "Live class created successfully!")
-        return redirect("list_live_classes", slug=course.slug)
-
-    return render(request, "liveclasses/create_live_class.html", {"course": course})
+    def get_success_url(self):
+        return reverse("list_live_classes", kwargs={"slug": self.kwargs['slug']})
 
 
-def join_live_class(request, meeting_id):
+class JoinLiveClassView(LoginRequiredMixin, View):
     """Render a Jitsi meeting inside LMS with JWT authentication."""
 
-    live_class = get_object_or_404(LiveClass, meeting_id=meeting_id)
-
-    return render(request, "liveclasses/join_live_class.html",
-                  {"live_class": live_class})
+    def get(self, request, meeting_id):
+        live_class = get_object_or_404(LiveClass, meeting_id=meeting_id)
+        return render(request, "liveclasses/join_live_class.html", {"live_class": live_class})
