111b364a2aa61dbcfec2b76219c0f79effb84c70
md-tonmoy007 <tonmoyhossainjifat313@gmail.com>
2025-04-13 17:31:26 +0600

    initial setup for progression

---
+++ mindjunkies/lecture/signals.py
@@ -0,0 +1,25 @@
+# mindjunkies/lecture/signals.py
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import LectureCompletion
+from mindjunkies.courses.models import Module
+from mindjunkies.lecture.models import Lecture
+
+@receiver(post_save, sender=LectureCompletion)
+def update_module_progression(sender, instance, created, **kwargs):
+    if created:
+        lecture = instance.lecture
+        user = instance.user
+
+        module = lecture.module
+        total_lectures = module.lecture_set.count()
+        print(total_lectures)
+        completed_lectures = module.lecture_set.filter(
+            lecturecompletion__user=user
+        ).distinct().count()
+
+        if total_lectures > 0:
+            percentage = int((completed_lectures / total_lectures) * 100)
+            module.progression = percentage
+            module.save()
