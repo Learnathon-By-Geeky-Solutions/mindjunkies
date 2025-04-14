9e01c4a175ca5ee096fa07c76d19eae64fd09911
md-tonmoy007 <tonmoyhossainjifat313@gmail.com>
2025-04-14 11:38:43 +0600

    sonarqube: fixed security hotspot

--- mindjunkies/lecture/apps.py
+++ mindjunkies/lecture/apps.py
@@ -5,6 +5,5 @@ class LectureConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "mindjunkies.lecture"
 
-
     def ready(self):
         import mindjunkies.lecture.signals
