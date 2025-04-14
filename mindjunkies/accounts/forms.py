4123910a0c67f0ba3fbb716c80f13d826bd25236
md-tonmoy007 <tonmoyhossainjifat313@gmail.com>
2025-04-14 11:54:05 +0600

    enhanced edit profile page

--- mindjunkies/accounts/forms.py
+++ mindjunkies/accounts/forms.py
@@ -1,7 +1,20 @@
+from django import forms
 from cloudinary.forms import CloudinaryFileField
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import Profile
+from .models import User
+
+
+
+class UserForm(ModelForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "first_name", "last_name")
+
 
 
 class ProfileUpdateForm(ModelForm):
