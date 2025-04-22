1f4284cfc64a13eca68f1124f2bf74a8f27bab39
Shafayet Sadi <shafayet.sadi@gmail.com>
2025-04-22 11:50:19 +0600

    added docker-compose

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,18 @@
 FROM python:3.13-slim-bookworm
 
+# install dependencies
 RUN apt-get update && apt-get install -y --no-install-recommends curl ca-certificates
+
+# install uv
 ADD https://astral.sh/uv/install.sh /uv-installer.sh
 RUN sh /uv-installer.sh && rm /uv-installer.sh
 ENV PATH="/root/.local/bin/:$PATH"
 
+# copy project files
 ADD . /app
 WORKDIR /app
 
 RUN uv sync --frozen
 RUN uv sync --group prod --frozen
 
-RUN chmod +x entrypoint.sh
-
-CMD ["./entrypoint.sh"]
+RUN chmod +x ./entrypoint.sh
--- migrate.sh
+++ migrate.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 
-DJANGO_SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"shafayet.sadi@gmail.com"}
+DJANGO_SUPERUSER_USERNAME=${DJANGO_SUPERUSER_USERNAME:-"admin"}
+DJANGO_SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"admin@gmail.com"}
+DJANGO_SUPERUSER_PASSWORD=${DJANGO_SUPERUSER_PASSWORD:-"adminpass"}
 cd /app/
 
 uv run python manage.py migrate --noinput || true
-uv run python manage.py createsuperuser --email "$DJANGO_SUPERUSER_EMAIL" --noinput || true
+uv run python manage.py createsuperuser --username "$DJANGO_SUPERUSER_USERNAME" --email "$DJANGO_SUPERUSER_EMAIL" --noinput || true
