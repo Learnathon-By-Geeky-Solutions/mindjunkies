2b128b482c94043ddadad69f866269dfbf83071b
Shafayet Sadi <shafayet.sadi@gmail.com>
2025-04-25 01:52:20 +0600

    fixed: tests

--- mindjunkies/live_classes/tests/test_models.py
+++ mindjunkies/live_classes/tests/test_models.py
@@ -1,8 +1,9 @@
 import pytest
 from django.conf import settings
-from django.utils.timezone import now
+from django.utils.timezone import now, make_aware
 from freezegun import freeze_time
 from model_bakery import baker
+from datetime import datetime
 
 from mindjunkies.accounts.models import User
 from mindjunkies.courses.models import Course
@@ -86,7 +87,7 @@ def test_live_class_str_method():
         teacher=teacher,
         course=course,
         topic="Intro to Django",
-        scheduled_at="2025-03-15 12:00:00",
+        scheduled_at=make_aware(datetime(2025, 3, 15, 12, 0, 0)),
     )
 
-    assert str(live_class) == "Intro to Django - Django Basics (2025-03-15 12:00:00)"
+    assert str(live_class) == "Intro to Django - Django Basics (2025-03-15 12:00:00+06:00)"
